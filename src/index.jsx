d6e2bd76d134813288bb20c56779aabd482d3148
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-10-25 08:38:24 +0200

    fix: Gradebook is called 'grades' for students

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 
 export default function (app, options) {
-    app.addRouteListener('course.gradebook', function () {
+    app.addRouteListener(['course.gradebook', 'course.grades'], function () {
         app.addReadyListener('#wrapper > div.ic-app-nav-toggle-and-crumbs > .ic-app-crumbs', function (el) {
             el.insertAdjacentHTML('afterend', '<div id="gradebook-disclaimer__flash"></div>');
             ReactDOM.render(<Flash options={options} />, document.getElementById('gradebook-disclaimer__flash'));
