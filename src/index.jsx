efac8c4d650b442a0870de51e40fc374927247c6
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-10-29 11:05:01 +0100

    Use more efficient container creation

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,10 @@ import './index.css';
 export default function (app, options) {
     app.addRouteListener(['course.gradebook', 'course.grades'], function () {
         app.addReadyListener('#wrapper > div.ic-app-nav-toggle-and-crumbs > .ic-app-crumbs', function (el) {
-            el.insertAdjacentHTML('afterend', '<div id="gradebook-disclaimer__flash"></div>');
-            ReactDOM.render(<Flash options={options} />, document.getElementById('gradebook-disclaimer__flash'));
+            let container = document.createElement('div');
+            
+            el.parentNode.insertBefore(container, el.nextSibling);
+            ReactDOM.render(<Flash options={options} />, container);
         });
     });
 }
