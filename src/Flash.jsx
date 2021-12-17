1246ebec72e77344698593f9d62822439caa546a
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-17 21:46:19 +0100

    Destructure props

--- src/Flash.jsx
+++ src/Flash.jsx
@@ -4,15 +4,15 @@ import Modal from './Modal';
 import styles from './index.module.css';
 
 
-export default function (props) {
+export default function ({ options: { message, modalLink, modalTitle, modalText } }) {
     return (
         <div id={styles.flash} className='ic-flash-info'>
             <div className='ic-flash__icon' aria-hidden='true'>
                 <i className='icon-info'></i>
             </div>
-            {props.options.message} {props.options.modalText &&
-                <Modal title={props.options.modalTitle} link={props.options.modalLink || 'More info...'}>
-                    {props.options.modalText}
+            {message} {modalText &&
+                <Modal title={modalTitle} link={modalLink || 'More info...'}>
+                    {modalText}
                 </Modal>
             }
         </div>
