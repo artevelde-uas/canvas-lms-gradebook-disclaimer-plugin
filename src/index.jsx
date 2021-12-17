ebb51ba491bc6be8c16fcbec3458f7af3b079923
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-17 13:48:33 +0100

    Optionally show on assignments page

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,13 @@ import Flash from './Flash';
 
 
 export default function (options) {
-    router.onRoute(['courses.gradebook', 'courses.grades'], async () => {
+    const routes = ['courses.gradebook', 'courses.grades'];
+
+    if (options.showOnAssignmentsPage) {
+        routes.push('courses.assignments');
+    }
+
+    router.onRoute(routes, async () => {
         const el = await dom.onElementReady('#wrapper > div.ic-app-nav-toggle-and-crumbs > .ic-app-crumbs');
 
         let container = document.createElement('div');
